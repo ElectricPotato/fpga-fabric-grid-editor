1b271dfb039174ae79611ae99a8a57f9d23b335b
ElectricPotato <mironandrzej1@gmail.com>
2025-09-20 03:43:02 +0100

    add routing lines

--- script.js
+++ script.js
@@ -21,6 +21,31 @@ function resetGrid() {
         div.addEventListener("mousedown",()=> onMouseDown(div));
         div.id = i;
 
+        let connY = 0; //how much the connections should be offset from the bottom edge of the cell to the centre
+        let connX = 30; //how much the connections should be offset from the side edge of the cell to the centre
+        //coordinates of the inputs and outputs to each cell
+        cell_in_positions = [[100 - connX, 100 - connY], [100 - connY, connX], [connX, connY], [connY, 100 - connX]];
+        cell_out_positions = [[connX, 100 - connY], [100 - connY, 100 - connX], [100 - connX, connY], [connY, connX]];
+
+        let svg_contents = '';
+
+        for (let cell_in_idx = 0; cell_in_idx < 4; cell_in_idx++) {
+            for (let cell_out_idx = 0; cell_out_idx < 4; cell_out_idx++) {
+
+                if (Math.floor(Math.random() * 2) == 0) continue;
+
+                let line_str = '<line x1="' + cell_in_positions[cell_in_idx][0]
+                + '%" y1="' + cell_in_positions[cell_in_idx][1]
+                + '%" x2="' + cell_out_positions[cell_out_idx][0]
+                + '%" y2="' + cell_out_positions[cell_out_idx][1]
+                + '%" style="stroke:red;stroke-width:2" />';
+
+                svg_contents += line_str;
+            }
+        }
+
+        div.innerHTML = '<svg>' + svg_contents + '</svg>'
+
         container.appendChild(div);
     }
 }
